4a7901ba7ca4dcc291395b12889359cfc95e3efb
Shikha <sbhatt10@uncc.edu>
2019-04-08 19:42:40 -0400

    Sign Up Class

--- app/src/main/java/com/example/inclass10/MainActivity.java
+++ app/src/main/java/com/example/inclass10/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.inclass10;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signUp = findViewById(R.id.buttonSignupMain);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Signup.class));
+                finish();
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/inclass10/Signup.java
+++ app/src/main/java/com/example/inclass10/Signup.java
@@ -2,12 +2,50 @@ package com.example.inclass10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class Signup extends AppCompatActivity {
+    EditText firstName, lastName, email, password, password02;
+    public  final String SIGN_UP_URL = "http://ec2-3-91-77-16.compute-1.amazonaws.com:3000/api/auth/register";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        firstName = findViewById(R.id.editTextFirstName);
+        lastName = findViewById(R.id.editTextLastName);
+        email = findViewById(R.id.editTestEmail);
+        password = findViewById(R.id.editTextPassword);
+        password02 = findViewById(R.id.editTextPasswordCom);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(SIGN_UP_URL)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("Demo", response.body().toString());
+
+            }
+        });
+
     }
 }
