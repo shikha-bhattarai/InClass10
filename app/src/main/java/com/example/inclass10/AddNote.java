e30dbd37ab99052001d3119a6f24932baf333afe
Teena Xiong <teenaxiong@gmail.com>
2019-04-09 11:24:11 -0400

    put length limit on text

--- app/src/main/java/com/example/inclass10/AddNote.java
+++ app/src/main/java/com/example/inclass10/AddNote.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,7 @@ import okhttp3.Response;
 public class AddNote extends AppCompatActivity {
     TextView editTextNote;
     String url = "http://ec2-3-91-77-16.compute-1.amazonaws.com:3000/api/note/post";
+    static final int MAX_LENGTH = 1000;
 
 
     @Override
@@ -38,6 +40,7 @@ public class AddNote extends AppCompatActivity {
         setContentView(R.layout.activity_add_note);
 
         editTextNote = findViewById(R.id.editTextNote);
+        editTextNote.setFilters(new InputFilter[]{new InputFilter.LengthFilter(MAX_LENGTH)});
 
         findViewById(R.id.buttonPostIt).setOnClickListener(new View.OnClickListener() {
             @Override
