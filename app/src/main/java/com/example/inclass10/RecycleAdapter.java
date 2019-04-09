86dd16704081ead133bf2febd00e34961d09cf8e
Shikha <sbhatt10@uncc.edu>
2019-04-09 15:32:19 -0400

    Delete Notes

--- app/src/main/java/com/example/inclass10/RecycleAdapter.java
+++ app/src/main/java/com/example/inclass10/RecycleAdapter.java
@@ -10,12 +10,26 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ViewHolder> {
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RecycleAdapter extends RecyclerView.Adapter <RecycleAdapter.ViewHolder> {
+
 
 
     ArrayList<NoteObject> arrayList;
@@ -51,8 +65,29 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ViewHold
         viewHolder.deleteIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences sharedPref = view.getContext().getSharedPreferences("token", Context.MODE_PRIVATE);
-                String string = sharedPref.getString("token", "default_value");
+                String messageId = arrayList.get(i).getMessageID();
+                final String DELETE_NOTE_URL = "http://ec2-3-91-77-16.compute-1.amazonaws.com:3000/api/note/delete?msgId="+messageId;
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(DELETE_NOTE_URL)
+                        .header("x-access-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjYWJlMjAwZDQ2ZWQ3MGNmNDA5NzZiMSIsImlhdCI6MTU1NDgzMjUzNSwiZXhwIjoxNTU0OTE4OTM1fQ.GV7lcjkMMP6vuGEjhIWC3Br0CoHbgZh6IWsvMslC6uI")
+                        .header("Content-Type","application/x-www-form-urlencoded")
+                        .build();
+
+                Log.d("demo", DELETE_NOTE_URL);
+                Log.d("demo2", request.toString());
+
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+
+                    }
+                });
             }
         });
     }
@@ -65,12 +100,10 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ViewHold
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView deleteIcon;
         TextView displayMessage;
-
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             displayMessage = itemView.findViewById(R.id.textViewNote);
-
-
+            deleteIcon = itemView.findViewById(R.id.imageButton);
         }
     }
 }
